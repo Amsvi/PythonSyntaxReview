b24868d0ec26c4ca6d0a64256d4c6d3123ec4807
Amsvi <mousavi.amirhosseein@gmail.com>
2021-06-25 16:38:56 -0400

    add dic

---
+++ dic.py
@@ -0,0 +1,7 @@
+#dictionary, bar asas key farakhoni mishe.
+d= {'Ali': 32,'zahra':43,'hasan':42 }
+print(d['Ali'],d['hasan'],d)
+d['ali'] = 36
+d['Ali'] = 35
+del d["Ali"]
+print(d)
---
+++ tuple.py
@@ -0,0 +1,11 @@
+#taple , dic ,loop, file ,def , class , exception, sqlite3, array,
+
+#tuple ( like list () [] ) is ordered immutable object.
+t1=(1,2,3,4,5)
+# in tuple for one element we should use ,
+t2= (0,)
+t3 = 1,2,3,4
+print(type(t3))
+print(t3[1:-1])
+
+
